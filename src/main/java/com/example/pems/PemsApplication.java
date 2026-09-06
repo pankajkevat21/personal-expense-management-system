97c209f1c456eae52f37da596beb153454b4ed88
Pankaj Kevat <pankajkevat21@gmail.com>
2026-09-06 18:53:47 +0530

    Add Spring Boot application

---
+++ src/main/java/com/example/pems/PemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PemsApplication.class, args);
+    }
+
+}
